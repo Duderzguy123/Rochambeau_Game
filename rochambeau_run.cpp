1e6b7d9c81a5eb150d3740390e488e4ff0ce3757
Duderzguy123 <101076795+Duderzguy123@users.noreply.github.com>
2023-09-18 14:29:39 -0700

    Add files via upload

---
+++ rochambeau_run.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "rochambeau.h"
+
+int main(){
+    cout << boolalpha;
+
+    rochambeau();
+
+    cout << endl;
+
+    return EXIT_SUCCESS;
+}
